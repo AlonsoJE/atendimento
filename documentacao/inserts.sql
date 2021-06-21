4d7f3a699b5a5632c67a2eaa97852dcf103edea8
zeck <joseeduardop.alonso@hotmail.com>
2021-06-20 21:01:21 -0300

    Arquivos potencialmente necessários

---
+++ documentacao/inserts.sql
@@ -0,0 +1,16 @@
+INSERT INTO medico () VALUES
+(0, "1987", "Cardiologista", "João Fernandes"),
+(0, "981273", "Ortopedista", "Maria Rodrigues"),
+(0, "09893", "Pediatra", "Franciso de Souza"),
+(0, "09809", "Clínico Geral", "Miguel Figueira");
+
+INSERT INTO paciente () VALUES
+(0,"1994-08-16","Rua Augusto Alves Pereira","26", "José Eduardo Alonso", "MASCULINO"),
+(0,"2001-01-22","Avenida dos estudantes","40", "Marcia Lacerda", "FEMININO"),
+(0,"1999-04-2","Rua Salvador Joaquin","200", "Larissa Domingues", "FEMININO"),
+(0,"1988-02-24","Rua 12","755", "Murilo Ricardo", "MASCULINO"),
+(0,"2009-07-12","Avenida Industrial","1", "Paulo Ricardo", "MASCULINO");
+
+INSERT INTO posto_coleta () VALUES
+(0, "Laboratório Central", "Rua Coronel Militão", "70"),
+(0, "Centro de Especialidades Teste", "Rua 7 de setembro", "17");
